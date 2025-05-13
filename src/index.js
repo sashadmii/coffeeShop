189e3c531457b9c58aa82045509fbf79b02c6b2d
sashadmii <alexandra.rychka@gmail.com>
2025-05-12 21:16:57 -0400

    added a no queue section and a slider

--- src/index.js
+++ src/index.js
@@ -1,2 +1,23 @@
 import './index.html';
 import './scss/main.scss';
+
+const slides = document.querySelectorAll('.carousel .card');
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+let currentSlide = 0;
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.toggle('active', i === index);
+  });
+}
+
+prev.addEventListener('click', () => {
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  showSlide(currentSlide);
+});
+
+next.addEventListener('click', () => {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+});
